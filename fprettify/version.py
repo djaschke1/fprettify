ace87a529d8bb98bee71e330d7837d13604e0561
Daniel Jaschke <daniel.jaschke@gmail.com>
2023-12-08 13:35:35 +0100

    Workaround failing version number import.

--- fprettify/version.py
+++ fprettify/version.py
@@ -5,6 +5,8 @@ except ModuleNotFoundError:
 try:
     __version__ = version(__package__)
 except PackageNotFoundError:
-    from setuptools_scm import get_version
-
-    __version__ = get_version(root="..", relative_to=__file__)
+    try:
+        from setuptools_scm import get_version
+        __version__ = get_version(root="..", relative_to=__file__)
+    except:
+        __version__ = "?.?.?"
